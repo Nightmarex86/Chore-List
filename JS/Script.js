cdac41cd2b672d3ff5ea3c1b1a09b7d4a2ecf11d
MichelKachouh <michelkachouh06@gmail.com>
2025-04-03 22:41:32 +0300

    added functionality

--- JS/Script.js
+++ JS/Script.js
@@ -1,17 +1,28 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-import { getDatabase, ref, push, onValue, snapshot,remove } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
+import { getDatabase, 
+    ref, 
+    remove, 
+    push,  
+    child, update, 
+    onValue } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
+
 const firebaseConfig = {
     databaseURL:"https://chore-list-c6a24-default-rtdb.europe-west1.firebasedatabase.app"
 }
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-const choreListRef = ref(db, "chores");
-const choreEl= document.getElementById("chores-el");
-const inputEl= document.getElementById("input-el");
-const addBtn= document.getElementById("add-btn");
-const cancelBtn= document.getElementById("cancel-btn");
+const dbRef = ref(db);
+const inputEl=document.getElementById("input-el");
+const addBtn=document.getElementById("add-btn");
+const cancelBtn=document.getElementById("cancel-btn");
+const choreEl=document.getElementById("chores-el");
+const choreListRef = ref(db, "chores/");
+addBtn.addEventListener("click",function(){
+    choreEl.innerHTML+=`<div>${inputEl.value}</div>`;
+    inputEl.value="";
 
-addBtn.addEventListener("click", function() {
-        push(choreListRef, inputEl.value);
-        inputEl.value = "";
+});
+
+cancelBtn.addEventListener("click",function(){
+    inputEl.value="";
 });
