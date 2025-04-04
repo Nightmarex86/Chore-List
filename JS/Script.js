c9ab3919f8125219ae0e9b9ee5c4fd2b0d882718
MichelKachouh <michelkachouh06@gmail.com>
2025-04-04 11:29:20 +0300

    added real time database with clear all button feature

--- JS/Script.js
+++ JS/Script.js
@@ -2,8 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebas
 import { getDatabase, 
     ref, 
     remove, 
-    push,  
-    child, update, 
+    push,
     onValue } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
 
 const firebaseConfig = {
@@ -11,18 +10,34 @@ const firebaseConfig = {
 }
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-const dbRef = ref(db);
+const dbRef = ref(db,"chores");
+
 const inputEl=document.getElementById("input-el");
 const addBtn=document.getElementById("add-btn");
 const cancelBtn=document.getElementById("cancel-btn");
 const choreEl=document.getElementById("chores-el");
-const choreListRef = ref(db, "chores/");
-addBtn.addEventListener("click",function(){
-    choreEl.innerHTML+=`<div>${inputEl.value}</div>`;
-    inputEl.value="";
+const clearBtn=document.getElementById("clear-btn");
 
+clearBtn.addEventListener("click",function(){
+    remove(dbRef);
+    choreEl.innerHTML="";
+}
+);
+
+addBtn.addEventListener("click",function(){
+    push(dbRef, inputEl.value);
+    inputEl.value="";
 });
 
 cancelBtn.addEventListener("click",function(){
     inputEl.value="";
+});
+
+onValue(dbRef, function(snapshot){
+    let chores=snapshot.val();
+    let choresArr=Object.values(chores);
+    choreEl.innerHTML="";
+    for(let i=0; i<choresArr.length; i++){
+        choreEl.innerHTML+=`<div>${choresArr[i]}</div>`;
+    }
 });
